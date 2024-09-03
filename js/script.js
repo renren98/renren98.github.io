eba63f0ee1d06bd074b6a3809fd48060c92e36b6
renren98 <160330649+renren98@users.noreply.github.com>
2024-09-03 12:30:56 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,6 +12,9 @@ buttons.forEach(button => {
             resultMessage.innerHTML = 'Congratulations! You won! <a href="https://sg.shp.ee/9ikyEKvp2woRajvyQcsR1">按这里赢取你的奖金</a>';
         } else {
             resultMessage.textContent = 'Sorry! Try again!';
+            setTimeout(() => {
+                window.location.href = 'https://renren98.github.io'; // Replace with the URL you want to redirect to
+            }, 1000); // Redirects after 2 seconds
         }
     });
 });
